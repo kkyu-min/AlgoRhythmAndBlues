81142cece8bb7b110c5647778ec8c17478863fe8
Kkyu-min <81220782+kkyu-min@users.noreply.github.com>
2023-12-14 21:20:55 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 128 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] tmp = br.readLine().split(" ");
+        int n = Integer.parseInt(tmp[0]);
+        int m = Integer.parseInt(tmp[1]);
+        int k = Integer.parseInt(tmp[2]);
+
+        int answer = 0;
+
+        while(true) {
+            // 조건 확인
+            if(n >= 2 && m >=1) {
+                if(n+m-3 >= k) {
+                    n-=2;
+                    m-=1;
+                    answer++;
+                }
+                else break;
+            }
+            else break;
+        }
+        System.out.println(answer);
+    }
+}
