b91f322ec2c4cfbfd568f3d8330b8ec3770e505c
Kkyu-min <81220782+kkyu-min@users.noreply.github.com>
2024-01-04 22:20:13 +0900

    [Gold V] Title: 등차수열과 쿼리, Time: 3240 ms, Memory: 312372 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/23888. 등차수열과 쿼리/등차수열과 쿼리.java
+++ 백준/Gold/23888. 등차수열과 쿼리/등차수열과 쿼리.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Main {
 
     static long a, d;
-
     static long gcd(long a, long b) {
         // a > b
         while(b != 0) {
@@ -17,35 +19,52 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] tmp = br.readLine().split(" ");
+//        String[] tmp = br.readLine().split(" ");
 
-        a = Integer.parseInt(tmp[0]);
-        d = Integer.parseInt(tmp[1]);
+//        a = Long.parseLong(tmp[0]);
+//        d = Long.parseLong(tmp[1]);
+        a = scanner.nextLong();
+        d = scanner.nextLong();
 
-        int q = Integer.parseInt(br.readLine());
+//        long q = Long.parseLong(br.readLine());
+        long q = scanner.nextLong();
 
         for(int i=0;i<q;i++) {
-            tmp = br.readLine().split(" ");
-            long con = Integer.parseInt(tmp[0]);
-            long l = Integer.parseInt(tmp[1]);
-            long r = Integer.parseInt(tmp[2]);
-            long al = a + ((l-1)*d);
-            long ar = a + ((r-1)*d);
+//            tmp = br.readLine().split(" ");
+//            int con = Integer.parseInt(tmp[0]);
+//            long l = Long.parseLong(tmp[1]);
+//            long r = Long.parseLong(tmp[2]);
+            int con = scanner.nextInt();
+            long l = scanner.nextLong();
+            long r = scanner.nextLong();
 
-            if(con == 1) { // 등차수열
-                long answer = ((r-l+1) * (al+ar)) /2;
-                System.out.println(answer);
+            if(con == 1) { //
+                long sum1 = (r * ((2*a) + (r-1)*d)) / 2;
+                long sum2 = ((l-1) * ((2*a) + (l-2)*d)) / 2;
+//                System.out.println(sum1-sum2);
+                String s = String.valueOf(sum1-sum2);
+                bw.write(s);
+                bw.newLine();
             }
             else { // 최대공약수
-                if(l == r) {
-                    System.out.println(al);
+                if(l==r) {
+//                    System.out.println(a+(l-1)*d);
+                    String s = String.valueOf(a+(l-1)*d);
+                    bw.write(s);
+                    bw.newLine();
                 }
-                else {
-                    System.out.println(gcd(a,d));
+                else{
+//                    System.out.println(gcd(a,d));
+                    String s = String.valueOf(gcd(a,d));
+                    bw.write(s);
+                    bw.newLine();
                 }
             }
         }
+        bw.close();
     }
 }
