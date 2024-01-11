9d54d64a3c828a9ef8d3a7f1ffd2a76c77359c13
Kkyu-min <81220782+kkyu-min@users.noreply.github.com>
2024-01-11 11:05:42 +0900

    [Silver II] Title: 창고 다각형, Time: 144 ms, Memory: 15072 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -3,34 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        int n =Integer.parseInt(br.readLine());
         int[] arr = new int[1001];
-
-        int answer = 0;
 
         for(int i=0;i<n;i++) {
             String[] tmp = br.readLine().split(" ");
-
             arr[Integer.parseInt(tmp[0])] = Integer.parseInt(tmp[1]);
         }
 
-        // 높이별 구간 탐색
-        for(int i=1;i<=1000;i++) {
-            int start = 1001;
-            int end = -1;
-            for(int j=1;j<1001;j++) {
-                if(arr[j] >=i) {
-                    start = Math.min(start, j);
-                    end = Math.max(end, j);
-                }
-            }
-//            System.out.println(start + " " + end);
-            if(start!=1001 && end!=-1){
-                answer +=(end-start+1);
-            }
+        int[] prefix = new int[1002];
+        prefix[0] = arr[0];
+        int[] suffix = new int[1002];
+        suffix[1001] = arr[1000];
+
+        for(int i=1;i<1001;i++) {
+            prefix[i] = Math.max(prefix[i-1], arr[i]);
+            suffix[1001-i] = Math.max(suffix[1002-i], arr[1001-i]);
+        }
+
+        int answer = 0;
+        for(int i=0;i<1002;i++) {
+            answer+=Math.min(prefix[i],suffix[i]);
         }
 
         System.out.println(answer);
