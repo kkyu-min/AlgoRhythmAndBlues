af42c9b25e0c417156cd043cb866def9a48e9c64
Kkyu-min <81220782+kkyu-min@users.noreply.github.com>
2024-01-24 19:07:37 +0900

    [Silver II] Title: Three Dots, Time: 864 ms, Memory: 31888 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.java
+++ 백준/Silver/13423. Three Dots/Three Dots.java
@@ -6,7 +6,24 @@ import java.util.HashSet;
 
 public class Main {
 
-    static HashSet<Integer> set;
+    //static HashSet<Integer> set;
+    static int[] arr;
+
+    private static boolean check(int start, int end) {
+        if((arr[start]+arr[end])%2!=0) {
+            return false;
+        }
+
+        int target = (arr[start]+arr[end])/2;
+        while(start <= end) {
+            int mid = (start+end)/2;
+
+            if(target==arr[mid]) return true;
+            else if(target < arr[mid]) end=mid-1;
+            else start=mid+1;
+        }
+        return false;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,22 +33,23 @@ public class Main {
 
         for(int t=0;t<test;t++) {
             int n = Integer.parseInt(br.readLine());
-            int[] arr = new int[n];
+            arr = new int[n];
 
-            set = new HashSet<>();
+            //set = new HashSet<>();
             String[] tmp = br.readLine().split(" ");
             for(int i=0;i<n;i++) {
                 arr[i] = Integer.parseInt(tmp[i]);
-                set.add(arr[i]);
+                //set.add(arr[i]);
             }
             Arrays.sort(arr);
 
             int cnt = 0;
             for(int i=0;i<n-2;i++){
                 for(int j=i+2;j<n;j++) {
-                    if((arr[j]-arr[i])%2==0) {
-                        if(set.contains((arr[j]+arr[i])/2)) cnt++;
-                    }
+//                    if((arr[j]-arr[i])%2==0) {
+//                        if(set.contains((arr[j]+arr[i])/2)) cnt++;
+//                    }
+                    if(check(i,j)) cnt++;
                 }
             }
 
