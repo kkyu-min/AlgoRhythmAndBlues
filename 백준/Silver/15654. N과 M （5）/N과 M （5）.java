e95b72d9fbcbb3a4c35cb701847aeb64bc0d4339
Kkyu-min <81220782+kkyu-min@users.noreply.github.com>
2024-02-02 10:07:37 +0900

    [Silver III] Title: N과 M (5), Time: 356 ms, Memory: 29072 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int n,m;
+    static int[] arr;
+    static int[] nums;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    static void recur(int cur) {
+        if(cur == m) {
+            for(int i=0;i<m;i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=0;i<n;i++) {
+            if(visited[i]) continue;
+            arr[cur] = nums[i];
+            visited[i] = true;
+            recur(cur+1);
+            visited[i] = false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] tmp = br.readLine().split(" ");
+
+        n = Integer.parseInt(tmp[0]);
+        m = Integer.parseInt(tmp[1]);
+
+        arr = new int[m];
+        nums = new int[n];
+        visited = new boolean[n];
+
+        tmp = br.readLine().split(" ");
+        for(int i=0;i<n;i++) {
+            nums[i] = Integer.parseInt(tmp[i]);
+        }
+        Arrays.sort(nums);
+
+        recur(0);
+        System.out.println(sb);
+    }
+}
